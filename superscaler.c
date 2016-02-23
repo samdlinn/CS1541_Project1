7382ff815ef2026194b41a9e1d83e24928f05db5
Joel McCoy <jvm13@pitt.edu>
2016-02-22 20:01:04 -0500

    Finished superscaler with added hazards

--- superscaler.c
+++ superscaler.c
@@ -18,8 +18,8 @@ static FILE *trace_fd;
 static int trace_buf_ptr;
 static int trace_buf_end;
 static struct trace_item *trace_buf;
-struct trace_item *IF[2]; //buffer array for instruction stages
-struct trace_item *ID[2]; //buffer array for instruction stages
+struct trace_item *IF_ID[2]; //buffer array for instruction stages
+struct trace_item *REG[2]; //buffer array for instruction stages
 struct trace_item *ALU[3]; //buffer array for ALU/Branch instruction stages
 struct trace_item *MEM[3]; //buffer array for MEM instructions
 
@@ -29,6 +29,8 @@ int read_next1 = 1; //if this is one read next instruction from file
 int read_next2 = 1; //if this is one read next instruction from file
 int branch_prediction_table[PREDICTION_TABLE_SIZE];
 int predict_status = 0; //used for 1 bit predictor 0 = predict not taken, 1 = predict taken
+int mem_after_branch_flag; //used for branch hazards
+int trace_view_on = 0;
 
 int is_big_endian(void)
 {
@@ -134,14 +136,14 @@ int print_buffers()
 {
 	int i;
 	printf("\n\t\t---Cycle Number: %d---\t\t\n", cycle_number);
-	printf("IF STAGE:\t");
-	print_item(&IF[0]);
+	printf("IF+ID STAGE:\t");
+	print_item(&IF_ID[0]);
 	printf("\t\t");
-	print_item(&IF[1]);
-	printf("ID STAGE:\t");
-	print_item(&ID[0]);
+	print_item(&IF_ID[1]);
+	printf("REG STAGE:\t");
+	print_item(&REG[0]);
 	printf("\t\t");
-	print_item(&ID[1]);
+	print_item(&REG[1]);
 	printf("ALU EX STAGE:\t");
 	print_item(&ALU[0]);
 	printf("MEM EX STAGE:\t");
@@ -174,6 +176,13 @@ int shift_MEM(struct trace_item **incoming)
 	MEM[0] = *incoming;		
 }
 
+//special shift for a branch condition hazard
+int shift_MEM_special(struct trace_item **incoming)
+{
+	MEM[2] = MEM[1];
+	MEM[1] = *incoming;		
+}
+
 //returns 1 when there is a dependency
 int is_dependent(struct trace_item **lw, struct trace_item **read)
 {
@@ -194,68 +203,7 @@ int is_dependent(struct trace_item **lw, struct trace_item **read)
 	return 0;
 }
 
-/*
-//function to check for data hazard and insert stall if necessary
-//returns 1 if there is a stall, 0 if no stall
-//if check is 0 buffer 1, if 1 buffer 2
-int data_hazard(struct trace_item **incoming, int check)
-{
-	if(!check)
-	{
-		//condition for lw stall with r-type instruction following it with dependency
-		if(buffer1[0]->type == 3 && (*incoming)->type == 1)
-				if(buffer1[0]->dReg == (*incoming)->sReg_a)
-					return 1;
-				else if(buffer1[0]->dReg == (*incoming)->sReg_b)
-					return 1;
-		
-		//condition for lw stall with i-type instruction following it with dependency
-		if(buffer1[0]->type == 3 && (*incoming)->type == 2)
-				if(buffer1[0]->dReg == (*incoming)->sReg_a)
-					return 1;
-					
-		//condition for lw stall with branch instruction following it with dependency
-		if(buffer1[0]->type == 3 && (*incoming)->type == 5)
-				if(buffer1[0]->dReg == (*incoming)->sReg_a)
-					return 1;
-				else if(buffer1[0]->dReg == (*incoming)->sReg_b)
-					return 1;
-					
-		//condition for lw stall with sw instruction following it with dependency
-		if(buffer1[0]->type == 3 && (*incoming)->type == 4)
-				if(buffer1[0]->dReg == (*incoming)->sReg_a)
-					return 1;
-	}
-	else
-	{
-		//condition for lw stall with r-type instruction following it with dependency
-		if(buffer2[0]->type == 3 && (*incoming)->type == 1)
-				if(buffer2[0]->dReg == (*incoming)->sReg_a)
-					return 1;
-				else if(buffer2[0]->dReg == (*incoming)->sReg_b)
-					return 1;
-		
-		//condition for lw stall with i-type instruction following it with dependency
-		if(buffer2[0]->type == 3 && (*incoming)->type == 2)
-				if(buffer2[0]->dReg == (*incoming)->sReg_a)
-					return 1;
-					
-		//condition for lw stall with branch instruction following it with dependency
-		if(buffer2[0]->type == 3 && (*incoming)->type == 5)
-				if(buffer2[0]->dReg == (*incoming)->sReg_a)
-					return 1;
-				else if(buffer2[0]->dReg == (*incoming)->sReg_b)
-					return 1;
-					
-		//condition for lw stall with sw instruction following it with dependency
-		if(buffer2[0]->type == 3 && (*incoming)->type == 4)
-				if(buffer2[0]->dReg == (*incoming)->sReg_a)
-					return 1;
-	}
-			
-	return 0;
-}
-*/
+
 //returns 0 if alu instruction
 //returns 1 if mem instruction
 int get_type(struct trace_item **incoming)
@@ -270,9 +218,9 @@ int get_type(struct trace_item **incoming)
 //returns 1 if there is a data hazard
 int check_data_hazard()
 {
-			if(ID[0]->type == 3)
+			if(REG[0]->type == 3)
 			{
-				if(is_dependent(&ID[0], &ID[1]))
+				if(is_dependent(&REG[0], &REG[1]))
 				{
 						return 1;
 				}
@@ -280,18 +228,104 @@ int check_data_hazard()
 			return 0;
 }
 
-/*
-//returns 1 if there is a control hazard with no prediction method
-int control_hazard_no_predict(struct trace_item **incoming)
+//return 1 if branch is taken
+int branch_taken(struct trace_item **prev, struct trace_item **cur)
 {
-	//there is a control hazard if next instruction incoming is resulted
-	//from branch and the branch is taken (due to predicting not taken)
-	if(buffer[0]->type == 5 && (*incoming)->PC != (buffer[0]->PC + 4))
+	if(((*prev)->PC - 4) == (*cur)->PC)
+		return 0;
+	else
 		return 1;
-	return 0;
 }
 
-//initializes the branch hash table to -1
+int check_branch_no_predict()
+{
+		if(ALU[0]->type == 5)
+		{
+				//case if the instruction is in the IF_ID stage
+
+					
+					if(mem_after_branch_flag)
+					{
+							if(branch_taken(&REG[0], &ALU[0]))
+							{
+								return 1;
+							}
+							
+					}
+					else
+					{
+							if(branch_taken(&MEM[0], &ALU[0]))
+							{
+								return 2;
+							}
+					}
+		}
+		return 0;
+}
+
+int check_branch_predict()
+{
+		int index;
+		int check;
+		
+		//if instruction in ALU(0) stage is branch check prediction table
+		if(ALU[0]->type == 5)
+		{
+			//bit masking of 10-4 bits of PC
+			index = (ALU[0]->PC) >> 4;
+			index = index & 0x0000007F; //gets bottom 7 bits
+			
+			//lookup index
+			check = branch_prediction_table[index];
+			//condition that branch prediction table index has predicted branch before
+			if(check == 1)
+			{
+				//now check if this instruction will branch
+				if((check_branch_no_predict() == 1) || (check_branch_no_predict() == 2)) //condition for it will branch
+				{
+					return 0; //no need to stall/squash--predicted correctly
+				}
+				else //condition for branch predictor failed and branch not taken
+				{
+					branch_prediction_table[index] = 0; //update table
+					return 1;
+				}
+			}
+			
+			//condition that branch prediction table index has predicted no branch before
+			else if (check == 0)
+			{
+				//now check if the instruction will not branch
+				if(check_branch_no_predict() == 0)//condition for no branch
+				{
+					return 0; // no need to stall/squash--predicted correctly
+				}
+				else
+				{
+					branch_prediction_table[index] = 1; //update table
+					return 1; //branch taken when not predicted not taken
+				}
+			}
+			
+			//condition for table index not initialized --predict not taken
+			else
+			{
+				if(check_branch_no_predict() == 0)//condition for no branch
+				{
+					branch_prediction_table[index] = 0; //update table
+					return 0; //no need to stall because branch predicted not taken
+				}
+				else //condition for branch taken
+				{
+					branch_prediction_table[index] = 1; //update table
+					return 1; //must stall because branch was predicted not taken
+				}
+			}
+		}
+		return 0;
+}
+
+
 int init_table()
 {
 	int i;
@@ -299,94 +333,28 @@ int init_table()
 		branch_prediction_table[i] = -1;
 }
 
-//returns 1 if there is a need to kill 2 instructions
-//due to branch resolution in the ex stage
-int control_hazard_predict(struct trace_item **incoming)
+//returns 1 if there is a jump hazard
+int jump_hazard()
 {
-	int index;
-	int check;
-	//if instruction in IF stage is branch check prediction table
-	if(buffer[0]->type == 5)
-	{
-		printf("Branch instruction PC is: %X\n", buffer[0]->PC);
-		//bit masking of 10-4 bits of PC
-		index = (buffer[0]->PC) >> 4;
-		index = index & 0x0000007F; //gets bottom 7 bits
-		printf("Index is %d\n", index);
+	if(ALU[0]->type == 6)
+		return 1;
 		
-		//lookup index
-		check = branch_prediction_table[index];
-		//condition that branch prediction table index has predicted branch before
-		if(check == 1)
-		{
-			//now check if this instruction will branch
-			if((buffer[0]->PC + 4) != (*incoming)->PC) //condition for it will branch
-			{
-				return 0; //no need to stall/squash--predicted correctly
-			}
-			else //condition for branch predictor failed and branch not taken
-			{
-				branch_prediction_table[index] = 0; //update table
-				return 1;
-			}
-		}
-		
-		//condition that branch prediction table index has predicted no branch before
-		else if (check == 0)
-		{
-			//now check if the instruction will not branch
-			if((buffer[0]->PC + 4) == (*incoming)->PC)//condition for no branch
-			{
-				return 0; // no need to stall/squash--predicted correctly
-			}
-			else
-			{
-				branch_prediction_table[index] = 1; //update table
-				return 1; //branch taken when not predicted not taken
-			}
-		}
-		
-		//condition for table index not initialized --predict not taken
-		else
-		{
-			if((buffer[0]->PC + 4) == (*incoming)->PC)//condition for no branch
-			{
-				branch_prediction_table[index] = 0; //update table
-				return 0; //no need to stall because branch predicted not taken
-			}
-			else //condition for branch taken
-			{
-				branch_prediction_table[index] = 1; //update table
-				return 1; //must stall because branch was predicted not taken
-			}
-		}
-	}
 	return 0;
 }
 
-//returns 1 if there is a jump hazard
-int jump_hazard(struct trace_item **incoming)
-{
-	//there is a jump instruction, must stall 1 cycle to calculate
-	//jump address
-	if(buffer[0]->type == 6)
-		return 1;
-	return 0;
-}*/
 
 int main(int argc, char **argv)
 {
 	struct trace_item *tr_entry1, *tr_entry2;
 	size_t size;
 	char *trace_file_name;
-	int trace_view_on = 0;
 	int branch_method = 0;
 	int i; //use for iterations
 	int hazard;
 	
 	
-	IF[2] = (struct trace_item*)malloc(sizeof(struct trace_item*) * 2);
-	ID[2] = (struct trace_item*)malloc(sizeof(struct trace_item*) * 2);
+	IF_ID[2] = (struct trace_item*)malloc(sizeof(struct trace_item*) * 2);
+	REG[2] = (struct trace_item*)malloc(sizeof(struct trace_item*) * 2);
 	ALU[3] = (struct trace_item*)malloc(sizeof(struct trace_item*) * 3);
 	MEM[3] = (struct trace_item*)malloc(sizeof(struct trace_item*) * 3);
   
@@ -424,18 +392,18 @@ int main(int argc, char **argv)
 	struct trace_item* noOp = (struct trace_item*)malloc(sizeof(struct trace_item*));
 	for (i = 0; i < 2; i++)
 	{
-		IF[i] = noOp;
-		ID[i] = noOp;
+		IF_ID[i] = noOp;
+		REG[i] = noOp;
 	}
 	for (i = 0; i < 3; i++)
 	{
 		ALU[i] = noOp;
 		MEM[i] = noOp;
 	}
-	/*
+	
 	//initialize branch prediction table if branch predict mode is on
 	if (branch_method == 1)
-		init_table();*/
+		init_table();
 		
 	//start of simulation
   
@@ -443,6 +411,7 @@ int main(int argc, char **argv)
 	read_next2 = 1;
 	while(1) 
 	{
+		mem_after_branch_flag = 0;
 		//will only read new instruction into tr_entry from the file
 		//only if there was not a stall from previous cycle
 		if(read_next1)
@@ -474,167 +443,179 @@ int main(int argc, char **argv)
 		else
 		{
 			hazard = 0;
-			//check for data_hazard
-			//condition for stall in both pipes 
-			/*
-			if ((data_hazard(&tr_entry1, 0) || data_hazard(&tr_entry2, 0)) && (data_hazard(&tr_entry1, 1) || data_hazard(&tr_entry2, 1)))
-			{
-				if (trace_view_on)
-					printf("\n\t\t---DATA HAZARD IN BOTH PIPES---\t\t\n");
-				shift_pipe1(&noOp); //shift pipe with NOOP (STALL)
-				shift_pipe2(&noOp);
-				read_next1 = 0; //indicates the next instruction will not be read
-				//will keep previous tr_entry for next loop to load into pipe
-			}
-			else if (data_hazard(&tr_entry1, 0) || data_hazard(&tr_entry2, 0))
-			{
-				if (trace_view_on)
-					printf("\n\t\t---DATA HAZARD IN FIRST PIPE---\t\t\n");
-				shift_pipe1(&noOp); //shift pipe with NOOP (STALL)
-				shift_pipe2(&tr_entry2);
-				read_next1 = 0; //indicates the next instruction will not be read
-				//will keep previous tr_entry for next loop to load into pipe
-			}
-			else if (data_hazard(&tr_entry1, 1) || data_hazard(&tr_entry2, 1))
-			{
-				if (trace_view_on)
-					printf("\n\t\t---DATA HAZARD IN SECOND PIPE---\t\t\n");
-				shift_pipe2(&noOp); //shift pipe with NOOP (STALL)
-				shift_pipe1(&tr_entry1);
-				read_next2 = 0; //indicates the next instruction will not be read
-				//will keep previous tr_entry for next loop to load into pipe
-			}/*
-			//condition for control hazard with prediction method turned off
-			else if (control_hazard_no_predict(&tr_entry) && branch_method == 0)
-			{
-				if(trace_view_on)
-					printf("\n\t\t---CONTROL HAZARD---\t\t\n");
-				shift_pipe(&noOp); //inserts squashed
-				cycle_number++; //counts for cycle inbetween
-				if(trace_view_on)
-					print_buffers();
-				
-				shift_pipe(&noOp);//inserts second squashed
-				read_next = 0;
-			}
-			else if(control_hazard_predict(&tr_entry) && branch_method == 1)
-			{
-				if(trace_view_on)
-					printf("\n\t\t---CONTROL HAZARD---\t\t\n");
-				shift_pipe(&noOp); //inserts squashed
-				cycle_number++; //counts for cycle inbetween
-				if(trace_view_on)
-					print_buffers();
-				
-				shift_pipe(&noOp);//inserts second squashed
-				read_next = 0;
-			}
-			else if(jump_hazard(&tr_entry))
-			{
-				if (trace_view_on)
-					printf("\n\t\t---JUMP HAZARD---\t\t\n");
-				shift_pipe(&noOp); //shift pipe with NOOP (STALL)
-				read_next = 0; //indicates the next instruction will not be read
-				//will keep previous tr_entry for next loop to load into pipe
-			}*/
+			
 			
 			//insert hazard conditions here
 			
-			//checks for data hazard --need to add condition for already in pipe
-			//only pushes one instruction if dependency
-			if(check_data_hazard())
+			if(jump_hazard())
+			{
+				if(trace_view_on)
+						printf("\n\t\t---JUMP HAZARD---\t\t\n");
+				shift_ALU(&noOp);
+				shift_MEM(&noOp);
+				cycle_number++;
+				if(trace_view_on)
+					print_buffers();
+				shift_ALU(&noOp);
+				shift_MEM(&noOp);
+			}
+			else if(check_branch_no_predict() == 2 && !branch_method)
+			{
+				if(trace_view_on)
+					printf("\n\t\t---JUMP HAZARD---\t\t\n");
+				shift_ALU(&noOp);
+				shift_MEM_special(&noOp);
+				cycle_number++;
+				if(trace_view_on)
+					print_buffers();
+				shift_ALU(&noOp);
+				shift_MEM_special(&noOp);
+			}
+			
+			//case for a branch condition
+			else if(check_branch_no_predict() == 1 && !branch_method)
+			{
+				if(trace_view_on)
+						printf("\n\t\t---CONTROL HAZARD---\t\t\n");
+				shift_ALU(&noOp);
+				shift_MEM(&noOp);
+				cycle_number++;
+				if(trace_view_on)
+					print_buffers();
+				shift_ALU(&noOp);
+				shift_MEM(&noOp);
+			}
+			else if(check_branch_no_predict() == 2 && !branch_method)
+			{
+				if(trace_view_on)
+					printf("\n\t\t---CONTROL HAZARD---\t\t\n");
+				shift_ALU(&noOp);
+				shift_MEM_special(&noOp);
+				cycle_number++;
+				if(trace_view_on)
+					print_buffers();
+				shift_ALU(&noOp);
+				shift_MEM_special(&noOp);
+			}
+			else if(check_branch_predict() && branch_method)
+			{
+					if(check_branch_no_predict() == 1)
+					{
+						if(trace_view_on)
+							printf("\n\t\t---CONTROL HAZARD---\t\t\n");
+						shift_ALU(&noOp);
+						shift_MEM(&noOp);
+						cycle_number++;
+						if(trace_view_on)
+							print_buffers();
+						shift_ALU(&noOp);
+						shift_MEM(&noOp);
+					}
+					else if(check_branch_no_predict() == 2)
+					{
+						if(trace_view_on)
+							printf("\n\t\t---CONTROL HAZARD---\t\t\n");
+						shift_ALU(&noOp);
+						shift_MEM(&noOp);
+						cycle_number++;
+						if(trace_view_on)
+							print_buffers();
+						shift_ALU(&noOp);
+						shift_MEM(&noOp);
+					}
+			}
+			else if(check_data_hazard())
 			{
 					//case for first pipe has a lw
-					shift_MEM(&ID[0]);
+					shift_MEM(&REG[0]);
 					shift_ALU(&noOp);
 					read_next1 = 0;
 					read_next2 = 1;
 					
-					ID[0] = ID[1];
-					ID[1] = IF[0];	
-					IF[0] = IF[1];
-					IF[1] = tr_entry2;
+					REG[0] = REG[1];
+					REG[1] = IF_ID[0];	
+					IF_ID[0] = IF_ID[1];
+					IF_ID[1] = tr_entry2;
 
 					
 					if(trace_view_on)
 						printf("\n\t\t---DATA HAZARD---\t\t\n");
 			}
 			//data hazard for when ahead
-			else if((MEM[0]->type == 3) && is_dependent(&MEM[0], &ID[0]))
+			else if((MEM[0]->type == 3) && is_dependent(&MEM[0], &REG[0]))
 			{
 				shift_ALU(&noOp);
 				shift_MEM(&noOp);
 				read_next1 = 0;
 				read_next2 = 0;
-				printf("\n\t\t---DATA HAZARD---\t\t\n");
+				if (trace_view_on)
+					printf("\n\t\t---DATA HAZARD---\t\t\n");
 			}
 			
 			
 			else //no hazard condition 
 			{
 				//case when both can be pushed into pipe
-				if(get_type(&ID[0]) != get_type(&ID[1]))
+				if(get_type(&REG[0]) != get_type(&REG[1]))
 				{
-						if(get_type(&ID[0]))
+						if(get_type(&REG[0]))
 						{
-							shift_MEM(&ID[0]);
-							shift_ALU(&ID[1]);
+							shift_MEM(&REG[0]);
+							shift_ALU(&REG[1]);
 						}
 						else
 						{
-							shift_MEM(&ID[1]);
-							shift_ALU(&ID[0]);
+							shift_MEM(&REG[1]);
+							shift_ALU(&REG[0]);
+							mem_after_branch_flag = 1;
 						}
 						read_next1 = 1;
 						read_next2 = 1;
 						
-						ID[0] = IF[0];
-						ID[1] = IF[1];
+						REG[0] = IF_ID[0];
+						REG[1] = IF_ID[1];
 						
-						IF[0] = tr_entry1;
-						IF[1] = tr_entry2;
+						IF_ID[0] = tr_entry1;
+						IF_ID[1] = tr_entry2;
 				}
-				else if(ID[0]->type == ID[1]->type && ID[1]->type == 0)
+				else if(REG[0]->type == REG[1]->type && REG[1]->type == 0)
 				{
-						shift_MEM(&ID[0]);
-						shift_ALU(&ID[1]);
+						shift_MEM(&REG[0]);
+						shift_ALU(&REG[1]);
 
 						read_next1 = 1;
 						read_next2 = 1;
 						
-						ID[0] = IF[0];
-						ID[1] = IF[1];
+						REG[0] = IF_ID[0];
+						REG[1] = IF_ID[1];
 						
-						IF[0] = tr_entry1;
-						IF[1] = tr_entry2;
+						IF_ID[0] = tr_entry1;
+						IF_ID[1] = tr_entry2;
 				}
 				//condition for same instruction types in ID stage
 				else
 				{
-						if(get_type(&ID[0]))
+						if(get_type(&REG[0]))
 						{
-								shift_MEM(&ID[0]);
+								shift_MEM(&REG[0]);
 								shift_ALU(&noOp);
 								read_next1 = 0;
 								read_next2 = 1;	
 						}
 						else
 						{
-								shift_ALU(&ID[0]);
+								shift_ALU(&REG[0]);
 								shift_MEM(&noOp);
 								
 								read_next1 = 0;
 								read_next2 = 1;
 						}
 						
-						ID[0] = ID[1];
-						ID[1] = IF[0];
+						REG[0] = REG[1];
+						REG[1] = IF_ID[0];
 						
-						IF[0] = IF[1];
-						IF[1] = tr_entry2;
-						
-
-						
+						IF_ID[0] = IF_ID[1];
+						IF_ID[1] = tr_entry2;
 						
 				}
 			}	
